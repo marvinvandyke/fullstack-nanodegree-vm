205965854028608687544bce3459396e30f92bae
marvinvandyke <dsagnotti@googlemail.com>
2019-12-19 22:19:15 +0100

    Lesson 2 complete

--- vagrant/restaurantmenu/webserver.py
+++ vagrant/restaurantmenu/webserver.py
@@ -20,9 +20,9 @@ def listRestaurants(self):
 
     for restaurantItem in restaurantItems:
         output+="<h1> %s </h1>" % restaurantItem.name
-        output+='''<a href='/restaurants/edit'>Edit</a>'''
+        output+='''<a href='/restaurants/%s/edit'>Edit</a>''' % restaurantItem.id
         output+="<br>"
-        output+='''<a href='/restaurants/delete'>Delete</a>'''
+        output+='''<a href='/restaurants/%s/delete'>Delete</a>''' % restaurantItem.id
 
     output+="<br>"
     output+='''<a href='/restaurants/new'><h1>Create new restaurant...</h1></a>'''
@@ -30,6 +30,59 @@ def listRestaurants(self):
     self.wfile.write(output)
     print(output)
     return
+
+def deleteRestaurant(self):
+    self.send_response(200)
+    self.send_header('Content-type', 'text/html')
+    self.end_headers()
+
+    arguments = str.split(self.path, "/")
+    restaurantid = int
+    for arg in arguments:
+        if arg.isdigit():
+            restaurantid=int(arg)
+
+    restaurantName = session.query(Restaurant).get(restaurantid).name
+
+    output = ""
+    output+="<html><body>"
+    output+='''<form method="POST" action="/restaurants/%s/delete" enctype="multipart/form-data"> 
+                     Really delete %s?<br>
+                    <input type="submit" value="Confirm">
+                </form>''' % (restaurantid, restaurantName)
+    output+="</body></html>"
+    self.wfile.write(output)
+
+    return
+
+def editRestaurant(self):
+    self.send_response(200)
+    self.send_header('Content-type', 'text/html')
+    self.end_headers()
+
+    print(self.path)
+    arguments = str.split(self.path, "/")
+    restaurantid = int
+    for arg in arguments:
+        if arg.isdigit():
+            restaurantid=int(arg)
+
+    print(restaurantid)
+    restaurantName = session.query(Restaurant).get(restaurantid).name
+    
+    output = ""
+    output+="<html><body>"
+    output+='''<form method="POST" action="/restaurants/%s/edit" enctype="multipart/form-data"> 
+                     Change %s to new name:<br>
+                    <input type="text" name="newRestaurantName">
+                    <br>
+                    <input type="submit" value="Change">
+                </form>''' % (restaurantid, restaurantName)
+    output+="</body></html>"
+    self.wfile.write(output)
+
+    return
+
 
 def createNewRestaurant(self):
     self.send_response(200)
@@ -63,12 +116,22 @@ class webServerHandler(BaseHTTPRequestHandler):
                 createNewRestaurant(self)
                 return
 
+            if self.path.endswith("/edit"):
+                editRestaurant(self)
+                return
+
+            if self.path.endswith("/delete"):
+                deleteRestaurant(self)
+                return
+
         except IOError:
             self.send_error(404, "File not found %s" % self.path)
 
     def do_POST(self):
         try:
             self.send_response(301)
+            self.send_header('Content-type', 'text/html')
+            self.send_header('Location', '/restaurants')
             self.end_headers()
 
             ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
@@ -77,7 +140,7 @@ class webServerHandler(BaseHTTPRequestHandler):
 
                 restaurantcontent=fields.get('restaurant')
                 print(restaurantcontent)
-                if len(restaurantcontent) > 0:
+                if restaurantcontent != None:
                     print("restaurant")
                     output = ""
                     output+="<html><body>"
@@ -95,6 +158,51 @@ class webServerHandler(BaseHTTPRequestHandler):
 
                     self.wfile.write(output)
                     print(output)
+                    return
+
+                newRestaurantNameContent=fields.get('newRestaurantName')
+                print(newRestaurantNameContent)
+                if newRestaurantNameContent != None:
+                    print("newRestaurantName")
+
+                    arguments = str.split(self.path, "/")
+                    restaurantid = int
+                    for arg in arguments:
+                        if arg.isdigit():
+                            restaurantid=int(arg)
+
+                    print(restaurantid)
+
+                    output = ""
+                    output+="<html><body>"
+                    output+='''<form method="POST" action="/restaurants/%s/edit" enctype="multipart/form-data"> 
+                                    Change %s to new name:<br>
+                                    <input type="text" name="newRestaurantName">
+                                    <br>
+                                    <input type="submit" value="Change">
+                                </form>''' % (restaurantid, newRestaurantNameContent[0])
+                    output+="</body></html>"
+
+                    rtc = session.query(Restaurant).get(restaurantid)
+                    rtc.name = newRestaurantNameContent[0]
+                    session.commit()
+
+                    self.wfile.write(output)
+                    print(output)
+                    return
+
+                arguments = str.split(self.path, "/")
+                restaurantid = int
+                for arg in arguments:
+                    if arg.isdigit():
+                        restaurantid=int(arg)
+
+                if self.path.endswith("/delete"):
+                    print("inside")
+                    rtc = session.query(Restaurant).get(restaurantid)
+                    print(rtc.name)
+                    session.delete(rtc)
+                    session.commit()
                     return
 
         except:
